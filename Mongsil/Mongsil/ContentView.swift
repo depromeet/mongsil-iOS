1bc289a73a5a3b1271b2093de5c31aa471993dd5
chanwoo <humains29@gmail.com>
2022-03-29 11:56:43 +0900

    feat: Add Project

---
+++ Mongsil/Mongsil/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mongsil
+//
+//  Created by Chanwoo Cho on 2022/03/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mongsil/Mongsil/MongsilApp.swift
@@ -0,0 +1,17 @@
+//
+//  MongsilApp.swift
+//  Mongsil
+//
+//  Created by Chanwoo Cho on 2022/03/29.
+//
+
+import SwiftUI
+
+@main
+struct MongsilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
