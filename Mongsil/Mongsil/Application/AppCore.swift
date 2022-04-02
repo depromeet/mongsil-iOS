68b3dc54ded0057374b8d6e71f86fde8ccd60033
Chanwoo Cho <72292617+GREENOVER@users.noreply.github.com>
2022-04-02 12:48:38 +0900

    feat: Initail view and appSceneDelegate and grouping (#7)

---
+++ Mongsil/Mongsil/Application/AppCore.swift
@@ -0,0 +1,28 @@
+//
+//  AppCore.swift
+//  Mongsil
+//
+//  Created by Chanwoo Cho on 2022/04/02.
+//
+
+import Combine
+import ComposableArchitecture
+
+struct AppState: Equatable {
+  
+}
+
+enum AppAction {
+  
+}
+
+struct AppEnvironment {
+  
+}
+
+let appReducer = Reducer<AppState, AppAction, AppEnvironment> {
+  state, action, env in
+  switch action {
+    
+  }
+}
---
+++ Mongsil/Mongsil/Module/Feature/Home/HomeView.swift
@@ -0,0 +1,8 @@
+//
+//  HomeView.swift
+//  Mongsil
+//
+//  Created by Chanwoo Cho on 2022/04/02.
+//
+
+import Foundation
