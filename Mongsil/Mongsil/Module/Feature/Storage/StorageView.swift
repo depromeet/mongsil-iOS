628764d551deb2221517d5905523bf63fdc4ceec
chanwoo <humains29@gmail.com>
2022-04-21 14:12:28 +0900

    feat: Add connect setting

--- Mongsil/Mongsil/Module/Feature/Storage/StorageView.swift
+++ Mongsil/Mongsil/Module/Feature/Storage/StorageView.swift
@@ -10,23 +10,58 @@ import ComposableArchitecture
 
 struct StorageView: View {
   private let store: Store<WithSharedState<StorageState>, StorageAction>
-
+  
   init(store: Store<WithSharedState<StorageState>, StorageAction>) {
     self.store = store
   }
-
+  
   var body: some View {
     VStack {
-      MSNavigationBar(
-        titleText: "보관함",
-        isUseBackButton: false,
-        rightButtonImage: R.CustomImage.settingIcon.image
-      )
+      StorageNavigationView(store: store)
       Spacer()
       Text("StoreView")
       Spacer()
     }
     .navigationTitle("")
     .navigationBarHidden(true)
+  }
+}
+
+private struct StorageNavigationView: View {
+  private let store: Store<WithSharedState<StorageState>, StorageAction>
+  
+  init(store: Store<WithSharedState<StorageState>, StorageAction>) {
+    self.store = store
+  }
+  
+  var body: some View {
+    ZStack {
+      MSNavigationBar(
+        titleText: "보관함",
+        isUseBackButton: false
+      )
+      HStack {
+        Spacer()
+        WithViewStore(store.scope(state: \.local.isSettingPushed)) { isSettingPushedViewStore in
+          NavigationLink(
+            destination: IfLetStore(
+              store.scope(
+                state: \.setting,
+                action: StorageAction.setting
+              ),
+              then: SettingView.init(store: )
+            ),
+            isActive: isSettingPushedViewStore.binding(
+              send: StorageAction.setSettingPushed
+            ),
+            label: {
+              R.CustomImage.settingIcon.image
+            }
+          )
+          .isDetailLink(true)
+          .padding(.trailing, 20)
+        }
+      }
+    }
   }
 }
